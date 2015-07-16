0637b5bea1ff8a792486067ccdea4917042d2a14
root <root@leo-pc.home>
2015-07-16 20:43:25 +0200

    keyboard

--- main.c
+++ main.c
@@ -6,10 +6,15 @@
 
 #define WIDTH_WINDOW 800
 #define HEIGHT_WINDOW 600
+#define SDL_Rekt SDL_Rect
+
+int move(int, SDL_Rect);
+
+
 
 int main(){
 	 
-	SDL_Event quit;
+	SDL_Event event;
 	SDL_Window* window;
 	SDL_Surface* screen;
 	SDL_Init(SDL_INIT_VIDEO);
@@ -17,8 +22,7 @@ int main(){
 				 SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
 				 WIDTH_WINDOW, HEIGHT_WINDOW,
 				 SDL_WINDOW_SHOWN);
-	
-	screen = SDL_GetWindowSurface(window);
+
 
 	SDL_Surface *snowMan1 = IMG_Load("snowmanB.png");
 	SDL_Surface *snowMan2 = IMG_Load("snowmanB.png");
@@ -36,22 +40,70 @@ int main(){
 	SDL_BlitSurface(snowMan2, NULL, screen, &positionSM2);
 	SDL_UpdateWindowSurface(window);
 
-	while(quit.type != SDL_QUIT) {
-	SDL_PollEvent(&quit);
+	while(event.type != SDL_QUIT) {
+	SDL_PollEvent(&event);
+		switch(event.key.keysym.sym){
 		
+		case SDLK_z:
+			positionSM2.y = move(1, positionSM2);
+			printf("position loop : %d \n", positionSM2.y);
+			usleep(10000);		
+		break;
+		case SDLK_q:
+			positionSM2.x = move(4, positionSM2); 
+			usleep(10000);
+
+		break;
+		case SDLK_s:
+			positionSM2.y = move(3, positionSM2);
+			usleep(10000);
+		break;
+		case SDLK_d:
+			positionSM2.x =  move(2, positionSM2);
+			usleep(10000);
+
+		break;
+		default:
+		break;
+
+
+		}
+	SDL_BlitSurface(snowMan2, NULL, screen, &positionSM2);	
+	SDL_UpdateRect(screen);
+	usleep(1000/30);
+
+
+	}
+	SDL_Quit();
+}
+
+
+int move(int direction, SDL_Rect position){
+
+	// direction : 1 haut, 2 : droite, 3 : bas, 4 gauche
+	switch(direction){
 	
-		
-
-
-
-
-	 
-	
+	case 1:
+		printf("position : %d \tdirection : %d \n", position.y, direction);
+		position.y ++;				
+		return position.y;
+	break;
+	case 2:
+		position.x ++;
+		return position.x;
+	break;
+	case 3:
+		position.y --;
+		return position.y;
+	break;
+	case 4:
+		position.x --;
+		return position.x;
+	break;
+	default:
+		return -1;
+	break;
 	}
 
-	SDL_Quit();
-
-
-
-
 }
+
